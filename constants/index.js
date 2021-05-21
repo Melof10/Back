e121b87038be8f26d08c15eec5f6a6f3787dc292
Melof10 <95fedemelo@gmail.com>
2021-05-21 09:06:59 -0300

    created constants

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    /***** API URL *****/
+    API_URL_POSTS: 'https://jsonplaceholder.typicode.com/posts',
+    API_URL_IMAGES: 'https://jsonplaceholder.typicode.com/photos',
+
+    /***** STATUS CODE *****/
+    SUCCESS_CODE: 200,
+    ERROR_CLIENT_CODE: 400,
+    ERROR_SERVER_CODE: 500,
+}
