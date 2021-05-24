ee9d9ab1fb5331e42f13cc646bf75c6b7fadb745
Melof10 <95fedemelo@gmail.com>
2021-05-23 21:51:48 -0300

    Added controller for request get post

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,4 @@
-const { getPosts } = require('../querys/post');
+const { getPosts, getPost } = require('../querys/post');
 const {
     SUCCESS_CODE,
     ERROR_CLIENT_CODE,
@@ -17,6 +17,31 @@ exports.getPosts = async(req, res) => {
                 status: SUCCESS_CODE,
                 message: SUCCESS_MESSAGE,
                 posts: posts
+            });
+        }else{
+            res.status(ERROR_CLIENT_CODE).send({
+                status: ERROR_CLIENT_CODE,
+                message: ERROR_DATA_NOT_FOUND_MESSAGE
+            })
+        }
+    } catch (error) {
+        res.status(ERROR_SERVER_CODE).send({
+            status: ERROR_SERVER_CODE,
+            message: ERROR_SERVER,
+            error: error
+        })
+    }
+}
+
+exports.getPost = async(req, res) => {
+    try {
+        const post = await getPost(req.params.id);
+                
+        if(post){
+            res.status(SUCCESS_CODE).send({
+                status: SUCCESS_CODE,
+                message: SUCCESS_MESSAGE,
+                post: post
             });
         }else{
             res.status(ERROR_CLIENT_CODE).send({
