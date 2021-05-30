bb73c90de861695ac86fdcff336d50c6ac029de6
Melof10 <95fedemelo@gmail.com>
2021-05-29 23:57:37 -0300

    Implemented requestResponse functions in post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,59 +1,24 @@
 const { getPosts, getPost } = require('../querys/post');
-const {
-    SUCCESS_CODE,
-    ERROR_CLIENT_CODE,
-    ERROR_SERVER_CODE,
-    SUCCESS_MESSAGE,
-    ERROR_SERVER,
-    ERROR_DATA_NOT_FOUND_MESSAGE 
-} = require('../constants');
+const { 
+    responseSuccess, 
+    responseErrorClient, 
+    responseErrorServer 
+} = require('../utils/responseRequest');
 
 exports.getPosts = async(req, res) => {
     try {
-        const posts = await getPosts();
-                
-        if(posts){
-            res.status(SUCCESS_CODE).send({
-                status: SUCCESS_CODE,
-                message: SUCCESS_MESSAGE,
-                posts: posts
-            });
-        }else{
-            res.status(ERROR_CLIENT_CODE).send({
-                status: ERROR_CLIENT_CODE,
-                message: ERROR_DATA_NOT_FOUND_MESSAGE
-            })
-        }
+        const posts = await getPosts();        
+        posts ? responseSuccess(res, posts) : responseErrorClient(res);                    
     } catch (error) {
-        res.status(ERROR_SERVER_CODE).send({
-            status: ERROR_SERVER_CODE,
-            message: ERROR_SERVER,
-            error: error
-        })
+        responseErrorServer(res, error);
     }
 }
 
 exports.getPost = async(req, res) => {
     try {
-        const post = await getPost(req.params.id);
-                
-        if(post){
-            res.status(SUCCESS_CODE).send({
-                status: SUCCESS_CODE,
-                message: SUCCESS_MESSAGE,
-                post: post
-            });
-        }else{
-            res.status(ERROR_CLIENT_CODE).send({
-                status: ERROR_CLIENT_CODE,
-                message: ERROR_DATA_NOT_FOUND_MESSAGE
-            })
-        }
+        const post = await getPost(req.params.id);                
+        post ? responseSuccess(res, post) : responseErrorClient(res);                    
     } catch (error) {
-        res.status(ERROR_SERVER_CODE).send({
-            status: ERROR_SERVER_CODE,
-            message: ERROR_SERVER,
-            error: error
-        })
+        responseErrorServer(res, error);
     }
 }
